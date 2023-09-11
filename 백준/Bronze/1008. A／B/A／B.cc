b556f4564fe2b58b3f5b180e66b4a4fc84dfe4a2
taetano <144678214+taetano@users.noreply.github.com>
2023-09-11 19:27:35 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+double a, b;
+
+int main() {
+    cin >> a >> b;
+    cout.precision(12);
+    cout << fixed;
+    cout << a / b << "\n";
+    return 0;
+}
