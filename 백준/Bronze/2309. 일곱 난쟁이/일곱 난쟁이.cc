0f0773e0ce04d2bd790e3aa07228f6d111c93546
taetano <144678214+taetano@users.noreply.github.com>
2023-09-15 23:50:18 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[9], sum;
+pair<int, int> ret;
+vector<int> v;
+void solve()
+{
+  for (int i = 0; i < 9; i++)
+  {
+    for (int j = 0; j < i; j++)
+    {
+      if (sum - a[i] - a[j] == 100)
+      {
+        ret = {i, j};
+        return;
+      }
+    }
+  }
+}
+int main()
+{
+  cin.tie(NULL);
+  cout.tie(NULL);
+  for (int i = 0; i < 9; i++)
+  {
+    cin >> a[i];
+    sum += a[i];
+  }
+  solve();
+  for (int i = 0; i < 9; i++)
+  {
+    if (ret.first == i || ret.second == i)
+    {
+      continue;
+    }
+    v.push_back(a[i]);
+  }
+  sort(v.begin(), v.end());
+  for (int i : v)
+    cout << i << "\n";
+  return 0;
+}
