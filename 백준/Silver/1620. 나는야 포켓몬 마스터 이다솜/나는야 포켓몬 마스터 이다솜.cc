9d164562dd408fa0954482b1c4d18ee017854f5d
taetano <144678214+taetano@users.noreply.github.com>
2023-09-20 13:10:31 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 328 ms, Memory: 25396 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, m;
+string s;
+map<string, int> mp;
+map<int, string> mp2;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin  >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        cin >> s;
+        mp[s] = i;
+        mp2[i] = s;
+    }
+    
+    for (int i = 0; i < m; i++) {
+        cin >> s;
+        if (atoi(s.c_str()) == 0) {
+            cout << mp[s] << "\n";
+        } else {
+            cout << mp2[atoi(s.c_str())] << "\n";
+        }
+    }
+
+    return 0;
+}
