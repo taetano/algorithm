4cb5d471489e2d47814618bb6ce0044998b84d1f
taetano <144678214+taetano@users.noreply.github.com>
2023-10-13 11:09:47 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -5,21 +5,13 @@ int total, a[9];
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    for (int i = 0; i < 9; i++) {
-        cin >> a[i];
-    }
-    
+    for (int i = 0; i < 9; i++) cin >> a[i];
     sort(a, a + 9);
     do {
         total = 0;
         for (int i = 0; i < 7; i++) total += a[i];
-        if (total != 100) continue;
-        
-        for (int i = 0; i < 7; i++) {
-            cout << a[i] << "\n";
-        }
-        break;
+        if (total == 100) break;
     } while (next_permutation(a, a + 9));
-    
+    for (int i = 0; i < 7; i++) cout << a[i] << "\n";
     return 0;
 }
