a2177665cc49ac57e4c9d71decde727cbdfc56f6
taetano <144678214+taetano@users.noreply.github.com>
2023-10-13 12:53:57 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,22 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, pos;
-string p, s, pre, suf;
+
+int n, targetIdx;
+string pre, suf, s;
 
 int main() {
-    cin >> n >> p;
-    int pos = p.find("*");
-    pre = p.substr(0, pos);
-    suf = p.substr(pos + 1);
-    while(n-- > 0) {
-        cin >> s;
-        if (s.size() < pre.size() + suf.size()) {
-            cout << "NE" << "\n";
-            continue;
-        }
-        
-        if (pre == s.substr(0, pre.size()) && suf == s.substr(s.size() - suf.size())) cout << "DA" << "\n";
-        else cout << "NE" << "\n";
-    }
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	cin >> s;
+	targetIdx = s.find("*");
+	pre = s.substr(0, targetIdx);
+	suf = s.substr(targetIdx + 1);
+	while (n--) {
+		cin >> s;
+		if (s.size() < pre.size() + suf.size()) cout << "NE" << "\n";
+		else {
+			string front = s.substr(0, pre.size());
+			string end = s.substr(s.size() - suf.size());
+			if (pre == front && suf == end) cout << "DA" << "\n";
+			else cout << "NE" << "\n";
+		}
+	}
+
     return 0;
 }
