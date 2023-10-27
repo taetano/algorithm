75e4f13868412bd0fc8cebcee741435076ea769d
taetano <144678214+taetano@users.noreply.github.com>
2023-10-27 10:35:59 +0900

    [Gold V] Title: 보물섬, Time: 140 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int dy[] = {-1,0,1,0}, dx[] = {0,1,0,-1};
+int n, m, mx, visited[54][54];
+char a[54][54];
+
+void bfs(int y, int x) {
+    memset(visited, 0, sizeof(visited));
+    visited[y][x] = 1;
+    queue<pair<int,int>> q;
+    q.push({y, x});
+    while(q.size()) {
+        tie(y, x) = q.front(); q.pop();
+        for (int i = 0; i < 4; i++) {
+            int ny = dy[i] + y;
+            int nx = dx[i] + x;
+            if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+            if (visited[ny][nx] || a[ny][nx] == 'W') continue;
+            visited[ny][nx] = visited[y][x] + 1;
+            q.push({ny, nx});
+            mx = max(mx, visited[ny][nx]);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    
+     for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] == 'L') bfs(i, j);
+        }
+    }
+    
+    cout << mx - 1;
+    return 0;
+}
