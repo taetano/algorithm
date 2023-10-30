b9f4f82b9d616dfcdab4a25c47f433a7bdfa3a40
taetano <144678214+taetano@users.noreply.github.com>
2023-10-30 11:11:47 +0900

    [Gold V] Title: 보물섬, Time: 120 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -2,26 +2,26 @@
 using namespace std;
 
 const int dy[] = {-1,0,1,0}, dx[] = {0,1,0,-1};
-int n, m, mx, visited[54][54];
+int visited[54][54], n, m, mx = -987654321;
+vector<pair<int,int>> land_list;
 char a[54][54];
 
 void bfs(int y, int x) {
-    memset(visited, 0, sizeof(visited));
-    visited[y][x] = 1;
-    queue<pair<int,int>> q;
-    q.push({y, x});
-    while(q.size()) {
-        tie(y, x) = q.front(); q.pop();
-        for (int i = 0; i < 4; i++) {
-            int ny = dy[i] + y;
-            int nx = dx[i] + x;
-            if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-            if (visited[ny][nx] || a[ny][nx] == 'W') continue;
-            visited[ny][nx] = visited[y][x] + 1;
-            q.push({ny, nx});
-            mx = max(mx, visited[ny][nx]);
-        }
-    }
+	visited[y][x] = 1;
+	queue<pair<int,int>> q;
+	q.push({y, x});
+	while (q.size()) {
+		tie(y, x) = q.front(); q.pop();
+		for (int i = 0; i < 4; i++) {
+			int ny = dy[i] + y;
+			int nx = dx[i] + x;
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+			if (visited[ny][nx] || a[ny][nx] == 'W') continue;
+			visited[ny][nx] = visited[y][x] + 1;
+			mx = max(mx, visited[ny][nx]);
+			q.push({ny, nx});
+		}
+	}
 }
 
 int main() {
@@ -30,15 +30,15 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> a[i][j];
+			if (a[i][j] == 'L') land_list.push_back({i, j});
         }
     }
     
-     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (a[i][j] == 'L') bfs(i, j);
-        }
-    }
-    
-    cout << mx - 1;
+    for (pair<int,int> land : land_list) {
+    	memset(visited, 0, sizeof(visited));
+    	bfs(land.first, land.second);
+	}
+	
+	cout << mx - 1;
     return 0;
 }
