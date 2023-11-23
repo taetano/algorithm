9e72ca769098ca67f4d9891de4586010e876f587
taetano <144678214+taetano@users.noreply.github.com>
2023-11-23 10:14:52 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 660 ms, Memory: 54584 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,27 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] sArr = br.readLine().split(" ");
-        int n = Integer.parseInt(sArr[0]);
-        int m = Integer.parseInt(sArr[1]);
-        int[] psum = new int[n + 1];
-        String[] arr = br.readLine().split(" ");
-        int index = 1;
-        for (String each : arr) {
-            psum[index] = psum[index - 1] + Integer.parseInt(each);
-            index++;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] pSum = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < n + 1; i++) {
+            pSum[i] = pSum[i - 1] + Integer.parseInt(st.nextToken());
         }
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            String[] temp = br.readLine().split(" ");
-            int start = Integer.parseInt(temp[0]);
-            int end = Integer.parseInt(temp[1]);
-            sb.append(psum[end] - psum[start - 1]).append(System.lineSeparator());
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            sb.append(pSum[end] - pSum[start - 1]).append(System.lineSeparator());
         }
 
         System.out.println(sb.toString());
